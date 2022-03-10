d83490ce39ba50fa1027d703ff2ef2afa39301cc
AnikKDev <anikdev2016@gmail.com>
2022-03-11 01:22:43 +0600

    fixed heading issue

--- js/app.js
+++ js/app.js
@@ -143,7 +143,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
-  document.getElementById("liked").innerHTML = "";
+  document.getElementById("liked").innerHTML = "" + `<h1>Liked posts</h1>`;
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -152,7 +152,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
-  document.getElementById("reported").innerHTML = "";
+  document.getElementById("reported").innerHTML = "" + `<h1>Reported posts</h1>`;
   const reportedPosts = getReportedPosts();
   reportedPosts.forEach((post) => {
     const div = createPost(post);
